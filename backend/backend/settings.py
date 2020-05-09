84835460c983cd0594e76a683ea5800caaab7b70
Angelico <angelico.delosreyes@gmail.com>
2020-05-10 01:43:35 +0800

    remove hard code

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,4 +131,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'backend')
+MEDIA_ROOT = os.path.join(BASE_DIR)
