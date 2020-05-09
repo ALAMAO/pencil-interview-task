9c3d5692fb74a229ca31c6968f179bba254f5669
Abel <abel.wong.2017@smu.edu.sg>
2020-05-10 00:06:00 +0800

    Add Max File Size Check

--- frontend/src/components/Upload/Upload.js
+++ frontend/src/components/Upload/Upload.js
@@ -8,55 +8,65 @@ import axios from 'axios';
 export default class Upload extends Component {
     constructor(props) {
         super(props);
+
+        this.onDropRejected = (files) => {
+            console.log(files)
+        }
+
         this.onDrop = (files) => {
-            var that = this;
-            this.setState({ files })
-            this.setState({ isLoading: true });
+            try {
+                var that = this;
+                this.setState({ files })
+                this.setState({ isLoading: true });
 
 
-            // Load File and convert to MD5 hash
-            var file = files[0]
-            var reader = new FileReader();
-            let formData = new FormData()
-            var img = new Image();
+                // Load File and convert to MD5 hash
+                var file = files[0]
+                var reader = new FileReader();
+                let formData = new FormData()
+                var img = new Image();
 
-            reader.onload = function (event) {
+                reader.onload = function (event) {
 
-                let width, height;
-                img.src = reader.result
+                    let width, height;
+                    img.src = reader.result
 
-                // get image dimensions
-                img.onload = function () {
-                    width = img.width
-                    height = img.height
+                    // get image dimensions
+                    img.onload = function () {
+                        width = img.width
+                        height = img.height
 
-                    formData.append("width", width)
-                    formData.append("height", height)
+                        formData.append("width", width)
+                        formData.append("height", height)
 
-                    //Send md5 hash AND file to DB
-                    // *To-do: Change the url here
-                    axios.post("https://httpbin.org/post", formData, {
-                        headers: {
-                            'Content-Type': 'multipart/form-data'
-                        }
-                    })
-                        .then(res => {
-                            console.log(res.data)
-                            that.setState({ isLoading: false });
-                            return that.props.history.push(`/output/${md5}`);
-
+                        //Send md5 hash AND file to DB
+                        // *To-do: Change the url here
+                        axios.post("https://httpbin.org/post", formData, {
+                            headers: {
+                                'Content-Type': 'multipart/form-data'
+                            }
                         })
-                        .catch(err => console.log(err))
-                }
+                            .then(res => {
+                                console.log(res.data)
+                                that.setState({ isLoading: false });
+                                return that.props.history.push(`/output/${md5}`);
 
-                var binary = event.target.result;
-                var md5 = CryptoJS.MD5(binary).toString();
+                            })
+                            .catch(err => console.log(err))
+                    }
 
-                formData.append("file", file)
-                formData.append("md5", md5)
+                    var binary = event.target.result;
+                    var md5 = CryptoJS.MD5(binary).toString();
 
-            };
-            reader.readAsDataURL(file);
+                    formData.append("file", file)
+                    formData.append("md5", md5)
+
+                };
+                reader.readAsDataURL(file);
+            }
+            catch (err) {
+                console.log(err)
+            }
         };
         this.state = {
             files: [],
@@ -64,11 +74,25 @@ export default class Upload extends Component {
         };
     }
 
+
     render() {
-        let dropZone = <Dropzone onDrop={this.onDrop}>
-            {({ getRootProps, getInputProps }) => (
+
+        let dropRej;
+
+        let dropZone = <Dropzone
+            onDropAccepted={this.onDrop}
+            onDropRejected={() => {
+                dropRej = <span className={classes.Danger}>
+                    File is too large, please try a smaller one
+                    </span>
+            }}
+            minSize={0}
+            maxSize={5242880}
+        >
+            {({ getRootProps, getInputProps, fileRejections }) => (
                 <section >
                     <div {...getRootProps({ className: 'dropzone' })} className={classes.Card}>
+
                         <img
                             src={`https://image.flaticon.com/icons/svg/1837/1837526.svg`}
                             width={"50px"}
@@ -79,10 +103,12 @@ export default class Upload extends Component {
                         <input {...getInputProps()} />
                         <p>Drag and drop a file here</p>
                         <div className={classes.break}></div>
+                        {dropRej}
+                        <div className={classes.break}></div>
                         {/* eslint-disable-next-line */}
                         <a className={classes.MockButton}>
                             <span className={classes.away}>Or click to select a file</span>
-                            <span className={classes.over}>Max File Size: 30MB</span>
+                            <span className={classes.over}>Max File Size: 5MB</span>
                         </a>
 
                     </div>
