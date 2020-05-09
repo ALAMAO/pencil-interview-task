bc01c9a60406f1728c8c08fabf7e3afb3d3fe42d
Abel <abel.wong.2017@smu.edu,.sg>
2020-05-09 18:33:21 +0800

    Add Axios Feature

--- frontend/src/components/Upload/Upload.js
+++ frontend/src/components/Upload/Upload.js
@@ -4,39 +4,46 @@ import React, { useCallback, useState } from 'react'
 import classes from './Upload.module.css'
 import CryptoJS from 'crypto-js';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios'
 
 
 export default function Upload() {
 
-    // specify upload params and url for your files
-    const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
 
     // called every time a file's `status` changes
     const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
 
     // receives array of files that are done uploading when submit button is clicked
     const handleSubmit = (files, allFiles) => {
-        // console.log(files.map(f => f.meta))
 
+        var reader = new FileReader();
         files.map(fileObj => {
 
             // console.log("This is the file Object", fileObj)
             // Load File and convert to MD5 hash
             var file = fileObj.file
-            var reader = new FileReader();
 
             reader.onload = function (event) {
                 var binary = event.target.result;
                 var md5 = CryptoJS.MD5(binary).toString();
-                console.log(md5)
+                fileObj.meta.md5 = md5
 
                 //Mock api to Send md5 hash AND fileObj to DB
-                setTimeout(() => {
-
-                }, 2000);
             }
-
             reader.readAsBinaryString(file);
+
+            let formData = new FormData()
+            formData.append("file", file)
+            formData.append("meta", JSON.stringify(fileObj.meta))
+
+            // Change the url here
+            axios.post("https://httpbin.org/post", formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+                .then(res => console.log(res.data))
+                .catch(err => console.log(err))
         })
 
         // Redirect to another page?
@@ -46,11 +53,11 @@ export default function Upload() {
     return (
         <div className={classes.Centre}>
             <Dropzone
-                getUploadParams={getUploadParams}
                 onChangeStatus={handleChangeStatus}
                 onSubmit={handleSubmit}
                 accept="image/*"
                 inputContent="Click to upload an image or drop a file"
+                style={{ dropzone: { overflow: "hidden" } }}
             />
 
 
