8dd899440e207e248da74939f95525f63e268121
Abel <abel.wong.2017@smu.edu.sg>
2020-05-10 15:49:35 +0800

    Create config files and UI touchups

--- frontend/src/components/Upload/Upload.js
+++ frontend/src/components/Upload/Upload.js
@@ -3,6 +3,7 @@ import Dropzone from 'react-dropzone';
 import classes from './Upload.module.css'
 import CryptoJS from 'crypto-js';
 import axios from 'axios';
+import config from './config';
 
 
 export default class Upload extends Component {
@@ -40,8 +41,7 @@ export default class Upload extends Component {
                         formData.append("height", height)
 
                         //Send md5 hash AND file to DB
-                        // *To-do: Change the url here
-                        axios.post("https://httpbin.org/post", formData, {
+                        axios.post(`${config.endpoints.dbPostURL}`, formData, {
                             headers: {
                                 'Content-Type': 'multipart/form-data'
                             }
@@ -76,8 +76,6 @@ export default class Upload extends Component {
 
 
     render() {
-        const minSizeInMB = 0;
-        const maxSizeInMB = 1;
 
         let dropRej;
 
@@ -94,10 +92,10 @@ export default class Upload extends Component {
 
                     switch (errorCode) {
                         case "file-invalid-type":
-                            errorMessages.push(<><span className={classes.Danger}>Please upload an image file of type jpg, jpeg or png </span> <div className={classes.break}></div></>)
+                            errorMessages.push(<><span className={classes.Danger}>{config.errorMessages.invalidFileMessage} </span> <div className={classes.break}></div></>)
                             break;
                         case "file-too-large":
-                            errorMessages.push(<><span className={classes.Danger}>File is too large. Max size: {maxSizeInMB} MB </span> <div className={classes.break}></div></>)
+                            errorMessages.push(<><span className={classes.Danger}>{config.errorMessages.fileSizeExceedMessage(config.sizeRestrictions.maxSizeInMB)}</span> <div className={classes.break}></div></>)
                             break;
                         default:
                             errorMessages.push(<><span className={classes.Danger}>"Unidentified Error" </span> <div className={classes.break}></div></>)
@@ -108,8 +106,8 @@ export default class Upload extends Component {
 
                 dropRej = errorMessages
             }}
-            minSize={1048576 * minSizeInMB}
-            maxSize={1048576 * maxSizeInMB}
+            minSize={1048576 * config.sizeRestrictions.minSizeInMB}
+            maxSize={1048576 * config.sizeRestrictions.maxSizeInMB}
             accept={'image/*'}
         >
             {({ getRootProps, getInputProps, fileRejections }) => (
@@ -133,7 +131,7 @@ export default class Upload extends Component {
                             <span className={classes.away}>Or click to select a file</span>
                             <span className={classes.over}>Max File Size: 5MB</span>
                         </a>
-
+                        <span className={classes.Info}>Max image size: {config.sizeRestrictions.maxSizeInMB}MB. Supported image types: jpg, jpeg or png </span>
                     </div>
 
                 </section>
@@ -147,9 +145,9 @@ export default class Upload extends Component {
                     Loader
                 </div>
                 <div className={classes.break}></div>
-                <h1 style={{ color: "rgb(0,37,53)" }}> Please Wait While We Classify Your Image</h1>
+                <h1 style={{ color: "rgb(0,37,53)" }}>{config.uiMessages.spinnerMessage}</h1>
                 <div className={classes.break}></div>
-                <span style={{ fontSize: "5rem" }} role="img" aria-label="sheep">🤗</span>
+                <span style={{ fontSize: "5rem" }} role="img" aria-label="sheep">{config.uiMessages.spinnerEmoji}</span>
             </>
         }
 
