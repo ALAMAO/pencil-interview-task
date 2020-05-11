016298490db10d1976525ca1873856af5ca542c1
mingshanhee <mingshan.hee@gmail.com>
2020-05-11 15:27:02 +0800

    Fixed Django reloading upon file changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xkph_4_xv_a9cx5q4icn=oi(cr**+5wgz&o94%d@ow&jan$h@6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", ["localhost", "127.0.0.1 [::1]"])
 
 
 # Application definition
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -15,4 +15,4 @@ python manage.py migrate
 
 exec "$@"
 
-python manage.py runserver 0.0.0.0:8000
+python -u manage.py runserver 0.0.0.0:8000
